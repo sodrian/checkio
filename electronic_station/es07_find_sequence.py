398091e6c4d82c9796d158a1ba2301fff1fb81b5
Dmitriy Berkutov <sodrian@gmail.com>
2016-01-23 03:19:04 +0300

    electronic station 07

--- electronic_station/es07_find_sequence.py
+++ electronic_station/es07_find_sequence.py
@@ -2,21 +2,50 @@ def checkio(matrix):
     for i, row in enumerate(matrix):
         for j, column in enumerate(row):
             try:
-                # to the left of the current element == 0 degrees
+                # to the right of the current element == 0 degrees
                 if matrix[i][j] == matrix[i][j+1] == matrix[i][j+2] == matrix[i][j+3]:
+                    print(matrix, 0)
                     return True
 
                 # 45 degrees
                 if matrix[i][j] == matrix[i+1][j+1] == matrix[i+2][j+2] == matrix[i+3][j+3]:
+                    print(matrix, 45)
+                    return True
+
+                # 90 degrees
+                if matrix[i][j] == matrix[i+1][j] == matrix[i+2][j] == matrix[i+3][j]:
+                    print(matrix, 90)
+                    return True
+
+                # 135 degrees
+                if matrix[i][j] == matrix[i+1][j-1] == matrix[i+2][j-2] == matrix[i+3][j-3]:
+                    print(matrix, 135)
+                    return True
+
+                # 180 degrees
+                if matrix[i][j] == matrix[i][j-1] == matrix[i][j-2] == matrix[i][j-3]:
+                    print(matrix, 180)
+                    return True
+
+                # 225 degrees
+                if matrix[i][j] == matrix[i-1][j-1] == matrix[i-2][j-2] == matrix[i-3][j-3]:
+                    print(matrix, 225)
+                    return True
+
+                # 270 degrees
+                if matrix[i][j] == matrix[i-1][j] == matrix[i][j-2] == matrix[i][j-3]:
+                    print(matrix, 270)
+                    return True
+
+                # 315 degrees
+                if matrix[i][j] == matrix[i-1][j+1] == matrix[i-2][j-2] == matrix[i-3][j-3]:
+                    print(matrix, 315)
                     return True
 
             except IndexError:
                 pass
 
-
-
-    #replace this for solution
-    return True or False
+    return False
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
