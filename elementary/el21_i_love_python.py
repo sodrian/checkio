e6a428a339bc7fd5fe3a37fbced9246c0e6d41de
Dmitriy Berkutov <sodrian@gmail.com>
2016-04-04 23:51:37 +0300

    el21

--- elementary/el21_i_love_python.py
+++ elementary/el21_i_love_python.py
@@ -4,6 +4,6 @@ def i_love_python():
     """
     return "I love Python!"
 
+
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
     assert i_love_python() == "I love Python!"
