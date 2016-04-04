adefef3e23c85d9c20fdbff9a703a7eaf3da9ddb
Dmitriy Berkutov <sodrian@gmail.com>
2016-04-04 23:50:35 +0300

    el15

--- elementary/el15_binary_count.py
+++ elementary/el15_binary_count.py
@@ -2,7 +2,6 @@ def checkio(number):
     return str(bin(number)).count('1')
 
 
-#These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     assert checkio(4) == 1
     assert checkio(15) == 4
