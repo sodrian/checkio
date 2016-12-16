cad6b35ab0f4e7146efcd9bd11c43738fbacb753
Dmitriy Berkutov <sodrian@gmail.com>
2016-12-17 02:51:46 +0300

    polishing

--- elementary/el21_i_love_python.py
+++
@@ -1,9 +0,0 @@
-def i_love_python():
-    """
-        Let's explain why do we love Python.
-    """
-    return "I love Python!"
-
-
-if __name__ == '__main__':
-    assert i_love_python() == "I love Python!"
